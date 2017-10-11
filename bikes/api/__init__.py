926b0019438e2f1bae20217c5d2810ceedf9d38d
DESKTOP-C2SO0GQ\NozzPisit <pisit.duangdee@hotmail.com>
2017-10-11 15:06:13 +0700

    +api_get_current_used_bike +CurrentUsedBikeSerializer

--- bikes/api/__init__.py
+++ bikes/api/__init__.py
@@ -2,4 +2,5 @@ __all__ = ["api_borrow_bike",
            "api_return_bike",
            "api_get_available_bikes",
            "api_get_bike_usage_plans",
-           "api_update_bike_location"]
+           "api_update_bike_location",
+           "api_get_current_used_bike"]
