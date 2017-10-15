fef9d71cef22b1614d9b304f1caf3e942cc3db8a
DESKTOP-C2SO0GQ\NozzPisit <pisit.duangdee@hotmail.com>
2017-10-16 06:39:38 +0700

    +@method_decorator

--- bikes/api/api_get_current_used_bike.py
+++ bikes/api/api_get_current_used_bike.py
@@ -1,19 +1,19 @@
 from django.utils.decorators import method_decorator
 from rest_framework.mixins import ListModelMixin
 
-from bikes.models import Bike
 from bikes.serializers import CurrentUsedBikeSerializer
 from histories.models import UserHistory
-from histories.serializers import UserHistorySerializer
-from kmitl_bike_django.decorators import token_required
 from kmitl_bike_django.utils import AbstractAPIView
+from kmitl_bike_django.decorators import token_required
+
+
 
 
 class GetCurrentUsedBikesView(AbstractAPIView, ListModelMixin):
     serializer_class = CurrentUsedBikeSerializer
     queryset = UserHistory.objects.filter(return_time=None)
 
-    # method_decorator(token_required)
+    @method_decorator(token_required)
     def get(self, request):
         return self.list(request)
 
